1dfb6fb61050bf5c439b3d8f3c2ff692d74780a1
yoruck2 <yoruck2@gmail.com>
2024-06-04 00:45:03 +0900

    배열을 enum - switch case 로 대체

--- TableViewControllerPractice/TableViewController.swift
+++ TableViewControllerPractice/TableViewController.swift
@@ -7,67 +7,53 @@
 
 import UIKit
 
-class TableViewController: UITableViewController {
+enum Preference: String, CaseIterable {
+    case entire = "전체 설정"
+    case personal = "개인 설정"
+    case etc = "기타"
     
-    var sectionTitleList: [String] = ["전체 설정", "개인 설정", "기타"]
-    var rowTitleList: [[String]] = [["공지사항", "실험실", "버전 정보"], ["개인/보안", "알림", "채팅", "멀티프로필"], ["고객센터/도움말"]]
+    var subOptions: [String] {
+        switch self {
+        case .entire:
+            return ["공지사항", "실험실", "버전 정보"]
+        case .personal:
+            return ["개인/보안", "알림", "채팅", "멀티프로필"]
+        case .etc:
+            return ["고객센터/도움말"]
+        }
+    }
+}
+
+class TableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "preferenceCell")
         self.navigationItem.title = "설정"
-
     }
+}
 
-
+extension TableViewController {
     override func numberOfSections(in tableView: UITableView) -> Int {
-
-        return sectionTitleList.count
+        return Preference.allCases.count
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-
-        return rowTitleList[section].count
-    }
-
     
-//    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-//        print(#function)
-//        return 80
-//    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Preference.allCases[section].subOptions.count
+    }
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        print(#function)
-            return sectionTitleList[section]
-        }
+        return Preference.allCases[section].rawValue
+    }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         print(#function)
-        let cell = tableView.dequeueReusableCell(withIdentifier: "preferenceCell")!
-        let text = rowTitleList[indexPath.section][indexPath.row]
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "preferenceCell") else {
+            return UITableViewCell()
+        }
+        let text = Preference.allCases[indexPath.section].subOptions[indexPath.row]
         
         cell.textLabel?.text = text
-//        cell.backgroundColor = .gray
-        
-//        if indexPath.row == 0 {
-//            cell.textLabel?.text = text
-//            cell.backgroundColor = .gray
-//        } else if indexPath.row == 1 {
-//            cell.textLabel?.text = text
-//            cell.textLabel?.textColor = .green
-//            cell.backgroundColor = .yellow
-//        } else if indexPath.row == 2 {
-//            cell.textLabel?.text = text
-//            cell.backgroundColor = .orange
-//        }
-        
         cell.textLabel?.font = .systemFont(ofSize: 11)
-//
-//        cell.detailTextLabel?.text = "ㅇㅇ"
-//        cell.detailTextLabel?.textColor = .blue
-//        cell.detailTextLabel?.font = .systemFont(ofSize: 10)
-        
-        print(#function)
         
         return cell
     }
